24ff1e4c15e5f07adc4439f66c4d075266bfaf6d
RodriguezRamiro <r.d.r@live.com>
2025-07-27 11:43:57 -0400

    fixed footer structure and spelling issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import '../styles/Footer.css';
 const Footer = () => {
     return (
         <footer className='footer'>
-            <p>© 2025 Ramiro Rodriguez ALvarez</p>
+            <p>© 2025 Ramiro Rodriguez Alvarez</p>
             <p>Version 1.0.0 - <a href="https://github.com/RodriguezRamiro/DevsLanding" target="_blank" rel="noopener noreferrer">View on GitHub</a></p>
         </footer>
     );
