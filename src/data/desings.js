0cc3dee69bce6fe085c0b37d8b6e33c20cea2c79
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-05-29 22:47:49 -0400

    included lates work assets  into designs for product showcase.

--- src/data/desings.js
+++ src/data/desings.js
@@ -3,6 +3,15 @@
 const projects = [
   {
     id: 1,
+    title: "Nexora Consulting",
+    description: "A modern operational framework, helping organizations connect, improve efficiency, visibility, and long-term growth.",
+    image: "/images/Nexora Consulting",
+    github: "https://github.com/RodriguezRamiro/MidMarketTemplate",
+    demo: "https://mid-market-template.vercel.app/index.html",
+    tags: ['React Framework', 'Javascript', 'CSS3'],
+  },
+  {
+    id: 2,
     title: "Clips Grooming",
     description: "A barber Shop inspired landing page for a real Small Bussiness Owner built with React and NodeJS.",
     image: "/images/ClipsGrooming.png",
@@ -11,7 +20,7 @@ const projects = [
     tags: ['React', 'NodeJS', 'CSS'],
   },
   {
-    id: 2,
+    id: 3,
     title: 'E-Commerce',
     description: 'Eshop desinged to browse products, manage a shopping cart, and complete secure checkout with flask backend and scalability of database and payment API integration.',
     image: '/images/pic.png',
@@ -20,7 +29,7 @@ const projects = [
     tags: ['React', 'Flask', 'CSS','JSON data', 'User Authentication' ],
   },
   {
-    id: 3,
+    id: 4,
     title: 'Snes Ui Lab',
     description: 'A browser-based retro console framework that can load and run cartridge like game desings using a consistent architecture.',
     image: '/images/snessUILab.png',
@@ -29,22 +38,13 @@ const projects = [
     tags: ['HTML', 'CSS', 'Javascript ES6'],
   },
   {
-    id: 4,
+    id: 5,
     title: 'API_Tester & Todo Dashboard',
     description: "Interactive API testing and Todo dashboard. Full-stack capabilities with a Flask backend, React frontend and live WebSocket",
     image:"./images/API_Tester.png",
     github: "https://github.com/RodriguezRamiro/apitester",
     demo: "https://apitester-fawn.vercel.app/",
     tags: ['React.js', 'Flask', 'Railway.app'],
-  },
-  {
-    id: 5,
-    title: 'Fullstack Blackjack',
-    description:'Fullstack Blackjack on React and Flask. Functional multiplayer and real-time chat using Flask-SocketIO.',
-    image: "/images/spotify-clone.png",
-    github: "https://github.com/RodriguezRamiro/fullstack_blackjack",
-    demo: "https://fullstack-blackjack.vercel.app/",
-    tags: ['React', 'Flask', 'WebSockets', 'CSS', 'Render'],
   },
   {
     id: 6,
@@ -54,13 +54,21 @@ const projects = [
     github: 'https://github.com/RodriguezRamiro/themedevstool',
     demo: 'https://themedevstool-hpwi.vercel.app/',
     tags: ['React', 'CSS Variables', 'Vite', 'JavaScript'],
-  }
+  },
 
 
 
   // Do not include Yet Project not finished..
   /*
-  {
+}
+      id: null,
+      title: 'Fullstack Blackjack',
+      description:'Fullstack Blackjack on React and Flask. Functional multiplayer and real-time chat using Flask-SocketIO.',
+      image: "/images/spotify-clone.png",
+      github: "https://github.com/RodriguezRamiro/fullstack_blackjack",
+      demo: "https://fullstack-blackjack.vercel.app/",
+      tags: ['React', 'Flask', 'WebSockets', 'CSS', 'Render'],
+  },
     id: 4,
     title: "Spotify Mock Clone",
     description: "A Spotify-inspired music streaming app built with React and Spotify API.",
